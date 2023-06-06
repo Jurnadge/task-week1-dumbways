1677d763c7a82e728b6f58185088a1071b1523f7
Jurnadge <chessarjunamariesto@gmail.com>
2023-06-06 12:02:35 +0700

    task-11

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"text/template"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,9 +10,76 @@ import (
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
-	})
+	e.Static("/public", "public")
+
+	e.GET("/", home)
+	e.GET("/contact", contact)
+	e.GET("/add-project", addproject)
+	e.GET("/my-testimonials", mytestimonials)
+
+	// quary string
+	// e.GET(project-detail/id:)
 
 	e.Logger.Fatal(e.Start("localhost:5000"))
 }
+
+func home(c echo.Context) error {
+	var tmpl, err = template.ParseFiles("views/index.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+
+	}
+
+	return tmpl.Execute(c.Response(), nil)
+}
+
+func contact(c echo.Context) error {
+	var tmpl, err = template.ParseFiles("views/contact.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+
+	}
+
+	return tmpl.Execute(c.Response(), nil)
+}
+
+func addproject(c echo.Context) error {
+	var tmpl, err = template.ParseFiles("views/add-project.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+
+	}
+
+	return tmpl.Execute(c.Response(), nil)
+}
+
+func mytestimonials(c echo.Context) error {
+	var tmpl, err = template.ParseFiles("views/my-testimonials.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+
+	}
+
+	return tmpl.Execute(c.Response(), nil)
+}
+
+// func projectDetail(c echo.Context) error {
+// 	id, _ := strconv.Atoi(c.Param("id"))
+
+// 	data := map[string]interface{}{
+// 		"id": id,
+// 		"Tittle": "aa"
+// 		"Content": "aa"
+// 	}
+// 	var tmpl, err = template.ParseFiles("views/project-detail.html")
+
+// 	if err != nil {
+// 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+// 	}
+
+// 	return tmpl.Execute(c.Response(), nil)
+// }
