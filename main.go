a16d2aa8fb162fba829ad64c35cad995642e02e3
Jurnadge <chessarjunamariesto@gmail.com>
2023-06-06 18:05:11 +0700

    task-11-updated

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
@@ -14,8 +15,10 @@ func main() {
 
 	e.GET("/", home)
 	e.GET("/contact", contact)
-	e.GET("/add-project", addproject)
-	e.GET("/my-testimonials", mytestimonials)
+	e.GET("/add-project", addProject)
+	e.POST("/added-project", addedProject)
+	e.GET("/my-testimonials", myTestimonials)
+	e.GET("/project-detail/:id", projectDetail)
 
 	// quary string
 	// e.GET(project-detail/id:)
@@ -45,7 +48,7 @@ func contact(c echo.Context) error {
 	return tmpl.Execute(c.Response(), nil)
 }
 
-func addproject(c echo.Context) error {
+func addProject(c echo.Context) error {
 	var tmpl, err = template.ParseFiles("views/add-project.html")
 
 	if err != nil {
@@ -56,7 +59,21 @@ func addproject(c echo.Context) error {
 	return tmpl.Execute(c.Response(), nil)
 }
 
-func mytestimonials(c echo.Context) error {
+func addedProject(c echo.Context) error {
+	title := c.FormValue("inputProjectName")
+	description := c.FormValue("inputDescription")
+	startDate := c.FormValue("inputStartDate")
+	endDate := c.FormValue("inputEndDate")
+
+	println("Tittle : " + title)
+	println("Description : " + description)
+	println("Start date : " + startDate)
+	println("End date :" + endDate)
+
+	return c.Redirect(http.StatusMovedPermanently, "/")
+}
+
+func myTestimonials(c echo.Context) error {
 	var tmpl, err = template.ParseFiles("views/my-testimonials.html")
 
 	if err != nil {
@@ -67,19 +84,19 @@ func mytestimonials(c echo.Context) error {
 	return tmpl.Execute(c.Response(), nil)
 }
 
-// func projectDetail(c echo.Context) error {
-// 	id, _ := strconv.Atoi(c.Param("id"))
+func projectDetail(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
 
-// 	data := map[string]interface{}{
-// 		"id": id,
-// 		"Tittle": "aa"
-// 		"Content": "aa"
-// 	}
-// 	var tmpl, err = template.ParseFiles("views/project-detail.html")
+	data := map[string]interface{}{
+		"id":      id,
+		"Tittle":  "Judul Project",
+		"Content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nam neque dicta sint, laudantium commodi iure sed! Ipsam corrupti at mollitia obcaecati maxime alias sequi laborum et! Maiores, nulla libero?",
+	}
+	var tmpl, err = template.ParseFiles("views/project-detail.html")
 
-// 	if err != nil {
-// 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
-// 	}
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
 
-// 	return tmpl.Execute(c.Response(), nil)
-// }
+	return tmpl.Execute(c.Response(), data)
+}
